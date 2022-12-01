01ad7cacbfc7b68f4dc000e423537ada24a6b555
a.gazalov <gazalov@list.ru>
2022-12-01 09:12:06 +0300

    Task 23 table of cubes from 1 till N

---
+++ Task23/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Clear();
+
+int DataEntry(string str)
+{
+    Console.Write(str);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+void Calc(int num)
+{
+    int i = 1;
+    Console.Write($"Таблица кубов чисел от 1 до {num}: ");
+    while (i <= num)
+    {
+        Console.Write(i * i * i + " ");
+        i++;
+    }
+}
+
+int numberN = DataEntry("Введите N: ");
+Calc(numberN);
